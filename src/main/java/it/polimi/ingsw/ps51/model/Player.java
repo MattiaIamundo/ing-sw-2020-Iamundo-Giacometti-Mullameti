6123a66aaba55ff3802099708e8fc1ac49d31290
meritamullameti <merita.mullameti@mail.polimi.it>
2020-03-30 15:59:16 +0200

    Implementing Player and Worker

--- src/main/java/it/polimi/ingsw/ps51/model/Player.java
+++ src/main/java/it/polimi/ingsw/ps51/model/Player.java
@@ -1,35 +1,58 @@
-
 package it.polimi.ingsw.ps51.model;
 
-
-
-
 import it.polimi.ingsw.ps51.model.Gods.Card;
-
 import java.util.List;
 
+/**
+ * @author Merita Mullameti
+ * This class is dedicated to the player
+ */
 public class Player {
 
     private String nickname;
     private Card god ;
     private List<Worker> workers;
 
+    /**
+     * The constructor of the class
+     * @param nickname represents the nickname of the player
+     */
     public Player (String nickname){
-
+        this.nickname=nickname;
     }
 
+    /**
+     * This method gets the card the player owns
+     * @return the card the player owns
+     */
     public Card getGod(){
-        return null;
+        return this.god;
     }
 
+    /**
+     * This method sets the card the player owns
+     * @param card represents the card the player owns
+     */
     public void setGod(Card card){
-
+        this.god=card;
     }
+
+    /**
+     * This method gets the workers the player owns
+     * @return the workers this player owns
+     */
     public List<Worker> getWorkers(){
-        return null;
+        return this.workers;
     }
 
+    /**
+     * This method sets the workers the player owns
+     * @param workers represents the workers this player owns
+     */
     public void setWorkers(List<Worker> workers){
-
+        this.workers=workers;
     }
 }
+
+
+
--- src/main/java/it/polimi/ingsw/ps51/model/Worker.java
+++ src/main/java/it/polimi/ingsw/ps51/model/Worker.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.ps51.model;
 
+/**
+ * @author Merita Mullameti
+ * This class is dedicated to the workers that each player possesses
+ */
 public class Worker{
 
     private String namePlayer;
@@ -7,23 +11,51 @@ public class Worker{
     private boolean canDoLevelUp;
 
 
-
-    public  Worker (String nickname){
+    /**
+     * The constructor of the class
+     * @param namePlayer represents the nickname of the Player
+     */
+    public  Worker (String namePlayer){
+        this.namePlayer=namePlayer;
     }
 
+    /**
+     * This method gets the nickname of the player
+     * @return the name of the player
+     */
     public String getNamePlayer(){
-        return null;
+        return this.namePlayer;
     }
-    public void move (Square pos){
 
+    /**
+     * This method sets the position of the worker
+     * @param pos represents the square where the worker is
+     */
+    public void setPosition (Square pos){
+        this.position=pos;
     }
+
+    /**
+     * This method gets the position of the worker
+     * @return the square where the worker is
+     */
     public Square getPosition(){
-        return null;
+        return this.position;
     }
-    public boolean getCanDoLevelUp(){
-        return false;
-    }
-    public void setCanDoLevelUp(boolean permission){
 
+    /**
+     * This method gets if the worker can build in this square
+     * @return if the worker can build in this square
+     */
+    public boolean getCanDoLevelUp(){
+        return this.canDoLevelUp;
+    }
+
+    /**
+     * This method sets if the worker can build in this square
+     * @param permission decides if the worker can build in the square
+     */
+    public void setCanDoLevelUp(boolean permission){
+        this.canDoLevelUp=permission;
     }
 }
