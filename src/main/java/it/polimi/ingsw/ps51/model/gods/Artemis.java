9905d0417eccad54ce029925094d350c12b6074a
meritamullameti <merita.mullameti@mail.polimi.it>
2020-04-07 08:31:09 +0200

    Artemis God

--- src/main/java/it/polimi/ingsw/ps51/model/gods/Artemis.java
+++ src/main/java/it/polimi/ingsw/ps51/model/gods/Artemis.java
@@ -1,4 +1,41 @@
 package it.polimi.ingsw.ps51.model.gods;
 
+import it.polimi.ingsw.ps51.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Artemis extends CommonAction{
+
+    @Override
+    public List<Coordinates> checkMoves(Worker worker, Map map) {
+        Square workerPosition = worker.getPosition();
+        List<Square> adjacentSquares = map.getAdjacentSquare(workerPosition);
+        List<Coordinates> validCoordinates = new ArrayList<>(super.checkMoves(worker,map));
+        boolean alreadyPresent = false;
+
+        for (Square square : adjacentSquares){
+
+            List<Square> adjacentSquares2 = map.getAdjacentSquare(square);
+            for(Square square2 : adjacentSquares2){
+                if ((square2 != null) && (square2.getLevel().ordinal() - square.getLevel().ordinal() <= 1)
+                            && (!square2.isPresentWorker()) && !square2.getLevel().equals(Level.DOME) && (square2 != workerPosition)){
+
+                    for (Coordinates coordinates : validCoordinates){
+                        if(coordinates == square2.getCoordinates()){
+                            alreadyPresent=true;
+                            break;
+                        }
+
+                    }
+
+                    if(!alreadyPresent)
+                        validCoordinates.add(square2.getCoordinates());
+
+                }
+            }
+        }
+
+        return clearPositions(validCoordinates, worker, map);
+    }
 }
---
+++ src/test/java/it/polimi/ingsw/ps51/model/gods/ArtemisTest.java
@@ -0,0 +1,99 @@
+package it.polimi.ingsw.ps51.model.gods;
+
+import it.polimi.ingsw.ps51.exceptions.OutOfMapException;
+import it.polimi.ingsw.ps51.model.Coordinates;
+import it.polimi.ingsw.ps51.model.Level;
+import it.polimi.ingsw.ps51.model.Map;
+import it.polimi.ingsw.ps51.model.Worker;
+import org.javatuples.Pair;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ArtemisTest {
+
+    private Artemis artemis;
+    private Worker worker;
+    private Map map;
+
+    @Before
+    public void setUp() {
+        this.artemis = new Artemis();
+        this.worker = new Worker("Merita");
+        this.map = new Map();
+    }
+
+    @After
+    public void tearDown() {
+        this.artemis = null;
+        this.worker = null;
+        this.map = null;
+    }
+
+    @Test
+    public void checkMovesTest() {
+
+        boolean containsAll = false;
+
+        try{
+
+            this.worker.setPosition(map.getSquare(1,1));
+            this.map.getSquare(1,1).setPresentWorker(this.worker);
+            this.map.getSquare(1,1).setLevel(Level.GROUND);
+
+            this.map.getSquare(0,0).setLevel(Level.GROUND);
+            this.map.getSquare(0,1).setLevel(Level.FIRST);
+            this.map.getSquare(0,2).setLevel(Level.THIRD);
+            this.map.getSquare(0,3).setLevel(Level.DOME);
+            this.map.getSquare(1,0).setLevel(Level.FIRST);
+            this.map.getSquare(1,2).setLevel(Level.FIRST);
+            this.map.getSquare(1,3).setLevel(Level.THIRD);
+            this.map.getSquare(2,0).setLevel(Level.DOME);
+            this.map.getSquare(2,1).setLevel(Level.GROUND);
+            this.map.getSquare(2,2).setLevel(Level.SECOND);
+            this.map.getSquare(2,3).setLevel(Level.FIRST);
+            this.map.getSquare(3,0).setLevel(Level.DOME);
+            this.map.getSquare(3,1).setLevel(Level.GROUND);
+            this.map.getSquare(3,2).setLevel(Level.FIRST);
+            this.map.getSquare(3,3).setLevel(Level.THIRD);
+
+
+
+            Worker worker2 = new Worker("Merita");
+            worker2.setPosition(map.getSquare(2,2));
+            this.map.getSquare(2,2).setPresentWorker(worker2);
+            this.map.getSquare(2,2).setLevel(Level.SECOND);
+
+            Worker worker3 = new Worker("Luca");
+            worker3.setPosition(map.getSquare(0,1));
+            this.map.getSquare(0,1).setPresentWorker(worker3);
+            this.map.getSquare(0,1).setLevel(Level.SECOND);
+
+
+        } catch (OutOfMapException e) {
+            e.printStackTrace();
+        }
+
+        List<Coordinates> validSquares = this.artemis.checkMoves(this.worker, this.map);
+        List<Coordinates> squares = new ArrayList<>();
+
+        squares.add(new Coordinates(0,0));
+        squares.add(new Coordinates(1,0));
+        squares.add(new Coordinates(1,2));
+        squares.add(new Coordinates(2,1));
+        squares.add(new Coordinates(2,3));
+        squares.add(new Coordinates(3,1));
+        squares.add(new Coordinates(3,2));
+
+        containsAll = squares.containsAll(validSquares);
+        Assert.assertTrue(containsAll);
+
+
+    }
+}
