e7b49736365fe283f9a4c3d5b5f36052def1610e
Luca <luca2.giacometti@mail.polimi.it>
2020-04-28 15:25:55 +0200

    bug fix: adding the serializable condition for all event

--- src/main/java/it/polimi/ingsw/ps51/events/events_for_client/EventForClient.java
+++ src/main/java/it/polimi/ingsw/ps51/events/events_for_client/EventForClient.java
@@ -1,9 +1,11 @@
 package it.polimi.ingsw.ps51.events.events_for_client;
 
+import java.io.Serializable;
+
 /**
  * Generic event that can be received by the client
  */
-public interface EventForClient {
+public interface EventForClient extends Serializable {
 
     void acceptVisitor(VisitorClient visitor);
 
--- src/main/java/it/polimi/ingsw/ps51/events/events_for_server/EventForServer.java
+++ src/main/java/it/polimi/ingsw/ps51/events/events_for_server/EventForServer.java
@@ -2,10 +2,12 @@ package it.polimi.ingsw.ps51.events.events_for_server;
 
 import it.polimi.ingsw.ps51.visitor.VisitorForPong;
 
+import java.io.Serializable;
+
 /**
  * Generic event that can be received by the server
  */
-public interface EventForServer {
+public interface EventForServer extends Serializable {
 
     void acceptVisitor(VisitorServer visitor);
 
