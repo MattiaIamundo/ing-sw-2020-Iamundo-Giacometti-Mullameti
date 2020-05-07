88257c44e5538c48d9d78a598647d292dcbd8abc
Luca <luca2.giacometti@mail.polimi.it>
2020-05-07 20:19:57 +0200

    [fix] adjust the javadoc of some classes of utility package

--- src/main/java/it/polimi/ingsw/ps51/utility/MessageHandler.java
+++ src/main/java/it/polimi/ingsw/ps51/utility/MessageHandler.java
@@ -9,6 +9,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+/**
+ * This class control if there is a new event from the server to signal it to the cli
+ */
 public class MessageHandler {
 
     private EventForClient event;
@@ -16,15 +19,27 @@ public class MessageHandler {
     VisitorClient visitorClient;
     ExecutorService ex = Executors.newSingleThreadExecutor();
 
+    /**
+     * Constructor
+     * @param s the supporter which has this as attribute
+     */
     public MessageHandler(Supporter s) {
         supporter = s;
         visitorClient = new VisitorView(s);
     }
 
+    /**
+     * Getter of ex
+     * @return The reference of the executor service used
+     */
     public ExecutorService getEx() {
         return this.ex;
     }
 
+    /**
+     * Here is tasked a new thread which controls if a new event is arrived from the server
+     * @return a future string which contains a string to indicate the type of the new event
+     */
     public Future<String> handleTheFuture() {
         return ex.submit( () -> {
 
--- src/main/java/it/polimi/ingsw/ps51/utility/PingThread.java
+++ src/main/java/it/polimi/ingsw/ps51/utility/PingThread.java
@@ -3,16 +3,28 @@ package it.polimi.ingsw.ps51.utility;
 import it.polimi.ingsw.ps51.events.events_for_client.Ping;
 import it.polimi.ingsw.ps51.network.server.socket.SocketConnection;
 
+/**
+ * This class represents a thread which send after an amount of time
+ * one {@link Ping} event to the client
+ */
 public class PingThread implements Runnable{
 
     SocketConnection sc;
     int halfTimeout;
 
+    /**
+     * Constructor
+     * @param socketConnection the socket connection which has this as attribute
+     * @param halfTimeout the amount of time to sleep before to send the ping event
+     */
     public PingThread(SocketConnection socketConnection, int halfTimeout) {
         this.sc = socketConnection;
         this.halfTimeout = halfTimeout;
     }
 
+    /**
+     * Sleep an amount of time and then send a ping event
+     */
     @Override
     public void run() {
 
